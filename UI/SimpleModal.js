b463089a9374c657137863bdcac52555330587bc
David Ra Youssef <davidradeveloper@gmail.com>
2021-01-03 08:45:29 -0500

    Add SimpleModal

---
+++ UI/SimpleModal.js
@@ -0,0 +1,114 @@
+class Modal {
+  constructor({ headerText = 'Are you sure?', contentText = '' }) {
+    this.headerText = headerText;
+    this.contentText = contentText;
+  }
+
+  handleClose() {
+    document.querySelector('.overlay').classList.remove('show');
+  }
+
+  handleClickOutside = event => {
+    if (!document.body.querySelector('.modal-container').contains(event.target)) {
+      this.handleClose();
+    }
+  };
+
+  addEventListeners = () => {
+    document.body.querySelector('.button__cancel').addEventListener('click', this.handleClose);
+    document.body.querySelector('.overlay').addEventListener('click', this.handleClickOutside);
+  };
+
+  render() {
+    const { headerText, contentText } = this;
+    const html = `
+      <div class="overlay">
+        <div class="modal-container">
+          <header>${headerText}</header>
+          ${contentText && '<div class="modal-content">' + contentText + '</div>'}
+          <footer>
+            <button class="button__cancel">CANCEL</button>
+            <button class="button__delete">DELETE</button>
+          </footer>
+        </div>
+      </div>
+    `;
+    document.body.innerHTML += html;
+    this.addEventListeners();
+  }
+}
+
+const modal = new Modal({
+  headerText: 'Are you sure you want to delete?',
+  contentText: 'This action cannot be undone.'
+});
+modal.render();
+renderOpenModalButton();
+renderStyles();
+
+function renderOpenModalButton() {
+  const button = document.createElement('button');
+  button.innerText = 'DELETE';
+  button.addEventListener('click', handleClick);
+  document.body.appendChild(button);
+  function handleClick() {
+    document.querySelector('.overlay').classList.add('show');
+  }
+}
+
+function renderStyles() {
+  const css = `
+    .overlay {
+      display: none;
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100vw;
+      height: 100vh;
+      background: rgba(0,0,0,.5);
+      align-items: center;
+      justify-content: center;
+    }
+    .overlay.show {
+      display: flex;
+    }
+    .modal-container {
+      width: 400px;
+      height: 200px;
+      background: white;
+      display: grid;
+      align-items: center;
+      box-shadow: 0 0 7px #555;
+      text-align: center;
+    }
+    .modal-container header {
+      font-size: 1.25rem;
+    }
+    .modal-content {
+      font-size: 1rem;
+    }
+    .modal-container footer {
+      justify-self: flex-end;
+      align-self: flex-end;
+      padding-right: 1rem;
+      padding-bottom: 1rem;
+      margin-top: -1rem;
+    }
+    .modal-container button {
+      padding: .5rem .75rem;
+      margin-left: .25rem;
+      border: none;
+      cursor: pointer;
+      color: white;
+    }
+    .modal-container .button__cancel {
+      background: #888;
+    }
+    .modal-container .button__delete {
+      background: #d9544f;
+    }
+  `;
+  const style = document.createElement('style');
+  style.appendChild(document.createTextNode(css));
+  document.head.appendChild(style);
+}
