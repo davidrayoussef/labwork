30ebe7538660da46b14d2bbe2e2c55cbde635ed6
David Ra Youssef <davidradeveloper@gmail.com>
2021-01-17 17:38:46 -0500

    Add Accordian

---
+++ UI/Accordian.js
@@ -0,0 +1,75 @@
+class Accordian {
+  constructor(data) {
+    this.data = data;
+  }
+
+  render() {
+    const containerDiv = document.createElement('div');
+    this.data.forEach(({ heading, content }) => {
+      const section = `
+        <section>
+          <h3>${heading}</h3>
+          <p class="collapse">${content}</p>
+        </section>
+      `;
+      containerDiv.innerHTML += section;
+    });
+    document.body.appendChild(containerDiv);
+    document.querySelectorAll('h3').forEach(heading => {
+      heading.addEventListener('click', handleHeadingClick);
+    });
+    this.renderStyle();
+  }
+
+  renderStyle() {
+    const css = `
+      h3 {
+        font-weight: normal;
+        font-size: 1.4rem;
+        background: #EEE;
+        padding: .2rem;
+        padding-left: 1rem;
+        border: 1px solid #AAA;
+        border-radius: 5px;
+        margin-bottom: .5rem;
+        cursor: pointer;
+      }
+      p {
+        font-size: 1rem;
+      }
+      .collapse {
+        display: none;
+      }
+      .expand {
+        display: block;
+      }
+    `;
+    const style = document.createElement('style');
+    style.appendChild(document.createTextNode(css));
+    document.head.appendChild(style);
+  }
+
+  handleHeadingClick(event) {
+    document.querySelectorAll('section p').forEach(p => {
+      if (p.textContent !== event.target.nextElementSibling.textContent) {
+        p.classList.remove('expand');
+        p.classList.add('collapse');
+      }
+    });
+    const p = event.target.nextElementSibling;
+    if (p.classList.contains('expand')) {
+      p.classList.remove('expand');
+      p.classList.add('collapse');
+    } else {
+      p.classList.remove('collapse');
+      p.classList.add('expand');
+    }
+  }
+}
+
+const accordian = new Accordian([
+  { heading: 'Heading A', content: 'Content A' },
+  { heading: 'Heading B', content: 'Content B' },
+  { heading: 'Heading C', content: 'Content C' }
+]);
+accordian.render();
